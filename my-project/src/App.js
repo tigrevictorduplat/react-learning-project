93d987bae75ce49e7647234b30f1f60a0dff1073
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2024-06-21 13:52:11 -0300

    main branch - Cleaned Up

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import Projeto from './components/Projeto';
-import Forms from './components/Forms';
-import MiniNotion from './components/mininotion/MiniNotion';
+
 
 function App() {
   return (
-    <div className="App">
-      <Projeto nome="meu primeiro projeto" dataInicio ="14/06/2024" dataFim="15/06/2024" contratante="Víctor D. Tigre" descricao="Um props com múltiplos atributos"/>
-      <Forms/>
-      <MiniNotion/>
-    </div>
+    <></>
   );
 }
 
